f30f77cd3c8af99b414ecafb18bbd030847248b2
lucia <lucia@slashstudio.cl>
2024-04-14 22:49:42 -0300

    libros con elecc compras

--- components/Libros.js
+++ components/Libros.js
@@ -9,34 +9,23 @@ const Libros = ({ detail }) => (
       <h2 className="titleBook">{detail.title}</h2>
       <p className="subtBook">Descripción:</p>
       <p className="descBook">{detail.description}</p>
+      <a className="buttonBuy" href={`/leer/${detail.linkLeer}`}>
+        Leer
+      </a>
+      <p className="subtBook mt-4">Métodos de compra:</p>
 
-      {detail.linkMP || detail.linkAmazon ? (
-        <>
-          <a className="buttonBuy" href={`/leer/${detail.linkLeer}`}>
-            Leer
-          </a>
-          <p className="subtBook mt-4">Métodos de compra:</p>
-          <div className="boxButtonsBook mt-1 mb-3">
-            {detail.linkAmazon ? (
-              <button className="buyAm">
-                <img alt="buyAm" className="" src={detail.linkAmazon} />
-              </button>
-            ) : null}
-            {detail.linkMP ? (
-              <button className="buyMP">
-                <img alt="buyMP" className="" src={detail.linkMP} />
-              </button>
-            ) : null}
-          </div>
-        </>
-      ) : (
-        <div className="boxButtonsBook">
-          <a className="buttonBuy" href={`/leer/${detail.linkLeer}`}>
-            Leer
-          </a>
-          <button className="buttonMore">Comprar</button>
-        </div>
-      )}
+      <div className="boxButtonsBook mt-1 mb-3">
+        {detail.linkAmazon ? (
+          <button className="buyAm">
+            <img alt="buyAm" className="" src={detail.linkAmazon} />
+          </button>
+        ) : null}
+        {detail.linkMP ? (
+          <button className="buyMP">
+            <img alt="buyMP" className="" src={detail.linkMP} />
+          </button>
+        ) : null}
+      </div>
     </div>
   </div>
 );
