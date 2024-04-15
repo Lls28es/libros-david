6aa4694f129d2c6744b4cb0a129b88c55de04f31
lucia <lucia@slashstudio.cl>
2024-04-14 22:34:23 -0300

    correc 3

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['localhost'],
-    // // domains: ['educomlab.com', 'firebasestorage.googleapis.com', 'localhost'],
   },
 };
 
