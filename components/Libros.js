896699042652cea4c7a5f66463a3f0cfa3703e6d
lucia <lucia@slashstudio.cl>
2024-04-24 21:08:06 -0300

    compra fisica o digital y stock

--- components/Libros.js
+++ components/Libros.js
@@ -1,9 +1,9 @@
 import Link from 'next/link';
+import { Tooltip } from '@nextui-org/react';
 
 const Libros = ({ detail }) => (
   <div id="boxBook" className="row m-4 px-4">
     <div className="col-4">
-      {console.log(detail)}
       <img alt="menu-mobile" className="imageBook w-100" src={detail.image} />
     </div>
     <div className="col-8 detailBook">
@@ -18,52 +18,97 @@ const Libros = ({ detail }) => (
         {detail.observation}
       </p>
 
-      <p className="detSubrBook">Descripción:</p>
-      <p className="detNormBook">{detail.description}</p>
-      {detail.observation === 'Disponible' ? (
-        <div className="detBook">
-          <p className="detSubrBook">Stock: </p>{' '}
-          <p className="detMargBook">{detail.stock}</p>
-        </div>
+      {detail.observation !== 'No publicado' ? (
+        <>
+          <p className="detSubrBook">Descripción:</p>
+          <p className="detNormBook">{detail.description}</p>
+
+          {detail.observation === 'Disponible' ? (
+            <div className="detBook">
+              <p className="detSubrBook">Stock Físico: </p>{' '}
+              <p className="detMargBook">{detail.stockFisico}</p>
+            </div>
+          ) : null}
+
+          {detail.observation === 'Disponible' ? (
+            <div className="detBook">
+              <p className="detSubrBook">Stock Digital: </p>{' '}
+              <p className="detMargBook">{detail.stockDigital}</p>
+            </div>
+          ) : null}
+
+          <div className="detBook">
+            <p className="detSubrBook">Año: </p>{' '}
+            <p className="detMargBook">{detail.year}</p>
+          </div>
+
+          <div className="d-flex pt-2 pb-4">
+            <a
+              className="buttonBuy buttonMl-0"
+              href={`/leer/${detail.linkLeer}?id=${detail.id}`}
+            >
+              Leer
+            </a>
+            <a
+              className="buttonBuy2"
+              href={`/leer/${detail.linkLeer}?id=${detail.id}`}
+            >
+              Leer
+            </a>
+            <a className="buttonBuy3" href={`/galeria/${detail.gallery.file}`}>
+              Galería
+            </a>
+          </div>
+
+          <p className="detSubrBook">Métodos de compra:</p>
+
+          <div className="boxButtonsBook my-2">
+            {detail.linkAmazon ? (
+              <Tooltip
+                content={'libro físico y digital'}
+                color="invert"
+                placement="bottom"
+                animated="false"
+                hideArrow="true"
+              >
+                <button className="buyAm buttonMl-0">
+                  <img alt="buyAm" className="" src="/amazon.png" />
+                </button>
+              </Tooltip>
+            ) : (
+              <button className="greyColor buyAm buttonMl-0">
+                <img alt="buyAm" src="/amazon.png" />
+              </button>
+            )}
+            {detail.linkMP ? (
+              <Tooltip
+                content={'libro digital'}
+                color="invert"
+                placement="bottom"
+                animated="false"
+                hideArrow="true"
+              >
+                <button className="buyMP">
+                  <img alt="buyMP" className="" src="Mercado-libre.jpg" />
+                </button>
+              </Tooltip>
+            ) : (
+              <button className="greyColor buyMP">
+                <img alt="buyMP" className="" src="Mercado-libre.jpg" />
+              </button>
+            )}
+          </div>
+          {/* <p className="fs-12 ff-gotham text-muted mt-5 mx-2">
+            {detail.linkAmazon && detail.linkAmazon
+              ? '* Amazon libro físico y digital. Mercado Pago libro digital'
+              : detail.linkAmazon
+              ? '* Amazon: libro físico y digital'
+              : detail.linkAmazon
+              ? '*Mercado Pago: libro digital'
+              : ''}
+          </p> */}
+        </>
       ) : null}
-
-      <div className="detBook">
-        <p className="detSubrBook">Año: </p>{' '}
-        <p className="detMargBook">{detail.year}</p>
-      </div>
-
-      <div className="d-flex pt-2">
-        <a
-          className="buttonBuy buttonMl-0"
-          href={`/leer/${detail.linkLeer}?id=${detail.id}`}
-        >
-          Leer
-        </a>
-        <a
-          className="buttonBuy2"
-          href={`/leer/${detail.linkLeer}?id=${detail.id}`}
-        >
-          Leer
-        </a>
-        <a className="buttonBuy3" href={`/galeria/${detail.gallery.file}`}>
-          Galería
-        </a>
-      </div>
-
-      <p className="detSubrBook mt-4">Métodos de compra:</p>
-
-      <div className="boxButtonsBook mt-1 mb-3">
-        {detail.linkAmazon ? (
-          <button className="buyAm  buttonMl-0">
-            <img alt="buyAm" className="" src="/amazon.png" />
-          </button>
-        ) : null}
-        {detail.linkMP ? (
-          <button className="buyMP">
-            <img alt="buyMP" className="" src="Mercado-libre.jpg" />
-          </button>
-        ) : null}
-      </div>
     </div>
   </div>
 );
--- components/ListaLibros.js
+++ components/ListaLibros.js
@@ -2,14 +2,13 @@ export default [
   {
     id: 0,
     title: 'MALVINAS UN PUEBLO SIN MEMORIA',
-    observation: 'Disponible',
+    observation: 'No disponible',
+    stockFisico: 0,
+    stockDigital: 'Disponible',
     description:
-      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     image: '/libros/malvinas-un-pueblo-sin-memoria.png',
     linkLeer: 'malvinas-un-pueblo-sin-memoria',
-    linkMP: 'Mercado Libre',
-    linkAmazon: 'Amazon',
-    stock: 1,
     year: 2019,
     gallery: {
       file: 'aviacion-naval-argentina?id=3',
@@ -64,13 +63,15 @@ export default [
     id: 1,
     title: 'A MINUTOS DEL FRENTE',
     observation: 'Disponible',
+    stockFisico: 'Disponible',
+    stockDigital: 'Disponible',
     description:
-      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     image: '/libros/a-minutos-del-frente.jpg',
     linkLeer: 'a-minutos-del-frente',
     linkMP: 'Mercado Libre',
     linkAmazon: 'Amazon',
-    stock: 1,
+
     year: 2022,
     gallery: {
       file: 'a-minutos-del-frente?id=1',
@@ -126,12 +127,13 @@ export default [
     title: 'LISTOS PARA LA BATALLA FUERZA AEREA ARGENTINA - parte 1',
     observation: 'Disponible',
     description:
-      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     image: '/libros/fuerza-aerea-argentina.jpg',
     linkLeer: 'fuerza-aerea-argentina',
     linkMP: 'Mercado Libre',
     linkAmazon: 'Amazon',
-    stock: 1,
+    stockFisico: 0,
+    stockDigital: 'Disponible',
     year: 2023,
     gallery: {
       file: 'fuerza-aerea-argentina?id=2',
@@ -185,14 +187,76 @@ export default [
   {
     id: 3,
     title: 'LISTOS PARA LA BATALLA AVIACIÓN NAVAL ARGENTINA - parte 2',
-    observation: 'No publicado',
+    observation: 'Disponible',
     description:
-      ' Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     image: '/libros/aviacion-naval-argentina.jpg',
     linkLeer: 'aviacion-naval-argentina',
     linkMP: 'Mercado Libre',
     linkAmazon: 'Amazon',
-    stock: 0,
+    stockFisico: 'Disponible',
+    stockDigital: 'Disponible',
+    year: 2024,
+    gallery: {
+      file: 'aviacion-naval-argentina?id=3',
+      amount: 47,
+    },
+    read: (
+      <>
+        <hr />
+        <p className="chapter">Capitulo 1: Introducción</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+        <hr />
+        <p className="chapter">Capitulo 2: Nudo</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+        <p>
+          Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+          dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </>
+    ),
+  },
+  {
+    id: 4,
+    title: 'LISTOS PARA LA BATALLA AVIACIÓN DE EJÉRCITO - parte 3',
+    observation: 'No publicado',
+    description: '',
+    image: '/libros/aviacion-de-ejercito.jpg',
+    linkLeer: 'aviacion-de-ejercito',
+    linkMP: 'Mercado Libre',
+    linkAmazon: 'Amazon',
+    stockFisico: 0,
+    stockDigital: 0,
     year: 2024,
     gallery: {
       file: 'aviacion-naval-argentina?id=3',
